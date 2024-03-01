29e2987d069568193da530ba8e9552ab6c232e79
Alok-2003 <alok953280@gmail.com>
2024-03-01 15:01:45 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,26 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-      Header
+      <NavContent />
     </div>
+  )
+}
+const NavContent = () => {
+  return (
+    <>
+      <h2>Alok </h2>
+      <div>
+        <a href="#home">Home</a>
+        <a href="#Work">Work</a>
+        <a href="#timeLine">Experience</a>
+        <a href="#services">Services</a>
+        <a href="#testimonial">Testimonial</a>
+        <a href="#contact">Contact</a>
+      </div>
+      <a href="mailto:alok953280@gmail.com">
+        <button>Email</button>
+      </a>
+    </>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {motion} from "framer-motion"
+
+const Home = () => {
+  return (
+    <div id='home'>
+      <section>
+        <div>
+            <motion.h1>
+                Hi, I Am <br/> Abhishek Singh
+            </motion.h1>
+        </div>
+      </section>
+      <section></section>
+    </div>
+  )
+}
+
+export default Home
