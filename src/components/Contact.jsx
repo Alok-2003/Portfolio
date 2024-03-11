d7e29bf7e58f8f476a61a6f97998587a91e7ea56
Alok-2003 <alok953280@gmail.com>
2024-03-11 12:33:44 +0530

    done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import vg from "../assets/vg.png";
-
+import toast from "react-hot-toast";
+import { motion } from "framer-motion";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase";
 
 const Contact = () => {
   const [name, setName] = useState("");
-
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [disableBtn, setDisableBtn] = useState(false);
 
   const submitHandler = async (e) => {
     e.preventDefault();
