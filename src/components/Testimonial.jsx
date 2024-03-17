23432bde1c98b2ed2c3b14d3c4dcaf7f3249692b
Alok-2003 <alok953280@gmail.com>
2024-03-17 23:40:10 +0530

    done

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -26,3 +26,16 @@ const Testimonial = () => {
     </div>
   );
 };
+
+const TestimonialCard = ({ name, feedback }) => (
+    <article>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+        alt="User"
+      />
+      <h4>{name}</h4>
+      <p>{feedback}</p>
+    </article>
+  );
+  
+  export default Testimonial;
