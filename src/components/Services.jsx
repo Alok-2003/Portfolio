73f54146c7c1b50db68167bf545a53277a146293
Alok-2003 <alok953280@gmail.com>
2024-03-20 13:20:06 +0530

    done

---
+++ src/components/Services.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { AiFillIeCircle, AiFillAndroid, AiFillWindows } from "react-icons/ai";
+
+const Services = () => {
+  const animations = {
+    whileInView: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+    },
+    one: {
+      opacity: 0,
+      x: "-100%",
+    },
+    twoAndThree: {
+      opacity: 0,
+      y: "-100%",
+    },
+
+    four: {
+      opacity: 0,
+      x: "100%",
+    },
+  };
