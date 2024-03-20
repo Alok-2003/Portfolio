77bb2430c1a267579f8dfb521b15ce613c31faf7
Alok-2003 <alok953280@gmail.com>
2024-03-20 13:20:26 +0530

    done

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -23,3 +23,48 @@ const Services = () => {
       x: "100%",
     },
   };
+  return (
+    <div id="services">
+      <h2>Services</h2>
+      <section>
+        <motion.div
+          className="serviceBox1"
+          whileInView={animations.whileInView}
+          initial={animations.one}
+        >
+          <h3>5+</h3>
+          <p>Years Experience</p>
+        </motion.div>
+        <motion.div
+          className="serviceBox2"
+          whileInView={animations.whileInView}
+          initial={animations.twoAndThree}
+        >
+          <AiFillIeCircle />
+          <span>Web Development</span>
+        </motion.div>
+        <motion.div
+          className="serviceBox3"
+          whileInView={animations.whileInView}
+          initial={animations.twoAndThree}
+          transition={{
+            delay: 0.2,
+          }}
+        >
+          <AiFillAndroid />
+          <span>App Development</span>
+        </motion.div>
+        <motion.div
+          className="serviceBox4"
+          whileInView={animations.whileInView}
+          initial={animations.four}
+        >
+          <AiFillWindows />
+          <span>Desktop Development</span>
+        </motion.div>
+      </section>
+    </div>
+  );
+};
+
+export default Services;
