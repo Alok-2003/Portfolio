cc7b9662fb32c3a6c29d4a903a32bc6e09c12abd
Alok-2003 <alok953280@gmail.com>
2024-03-24 23:49:40 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({ setMenuOpen, menuOpen }) => {
 
       <button className="navBtn" onClick={() => setMenuOpen(!menuOpen)}>
         <AiOutlineMenu />
-      </button>
+      </button> 
     </>
   );
 };
